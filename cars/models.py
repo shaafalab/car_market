dd533aa51998001460ea6b98402ec62ff376a619
mac <eng.abdullahshafaal@gmail.com>
2019-02-14 16:43:07 +0300

    mini project3

--- cars/models.py
+++ cars/models.py
@@ -5,9 +5,14 @@ class Car(models.Model):
 	make = models.CharField(max_length=120)
 	model = models.CharField(max_length=120)
 	year = models.IntegerField()
+	car_img = models.ImageField(null= True, blank=True)
 
 	def __str__(self):
 		return "{} {} - {}".format(self.make, self.model, self.year)
 
 	def get_absolute_url(self):
 		return reverse('car-detail', kwargs={'car_id':self.id})
+
+
+
+	
--- cars/views.py
+++ cars/views.py
@@ -1,32 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def car_list(request):
-	cars = Car.objects.all()
-	context = {
-		"cars": cars,
-	}
-	return render(request, 'car_list.html', context)
+    cars = Car.objects.all()
+    context = {
+        "cars": cars,
+    }
+    return render(request, 'car_list.html', context)
 
 
 def car_detail(request, car_id):
-	car = Car.objects.get(id=car_id)
-	context = {
-		"car": car,
-	}
-	return render(request, 'car_detail.html', context)
+    car = Car.objects.get(id=car_id)
+    context = {
+        "car": car,
+    }
+    return render(request, 'car_detail.html', context)
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
-
+    form = CarForm()
+    if request.method == "POST":
+            form = CarForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'successfly created.')
+            return redirect('car-list')
+    context = {
+        "form": form,
+    }
+    return render(request, 'car-create.html', context)
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+    car_obj = Car.objects.get(id=car_id)
+    form = CarForm(instance=car_obj)
+    if request.method == "POST":
+        form = CarForm(request.POST, instance=car_obj)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'successfly created.')
+            return redirect('car-list')
+    context = {
+        "car_obj": car_obj,
+        "form": form,
+    }
+    return render(request, 'car-update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+    car_obj = Car.objects.get(id=car_id)
+    car_obj.delete()
+    messages.success(request, 'successfly deleted.')
+    return redirect('car-list')
+
+
